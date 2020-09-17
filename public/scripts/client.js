079598a5109f1881382ae1783317fbbd270d6414
Casey Joy <67843601+caseyjoy@users.noreply.github.com>
2020-09-17 19:32:14 +0000

    Added ajax to load tweets, made it so you can add a new tweet, fixed some styles, fixed profile image loading

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,95 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+/*
+        <article class="tweet">
+          <header>
+            <div>
+              <img src="" class="user-icon">
+              <span class="tweet-name">John Smith</span>
+            </div>
+            <span class="tweet-username">@JohnSmith</span>
+          </header>
+          <div class="tweet-content">Is the world spinning... or am I?</div>
+          <footer>
+            <span class="tweeted-how-many-days-ago">10 Days ago</span>
+            <span class="tweet-buttons">⚐↻❤</span>
+          </footer>
+        </article>
+*/
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
+
+
+function createTweetElement(tweet) {
+  const newTweet = `<article class="tweet">
+                      <header>
+                        <div>
+                          <img src="${tweet.user.avatars}" class="user-icon">
+                            <span class="tweet-name">${tweet.user.name}</span>
+                        </div>
+                        <span class="tweet-username">${tweet.user.handle}</span>
+                      </header>
+                      <div class="tweet-content">${tweet.content.text}</div>
+                      <footer>
+                        <span class="tweeted-how-many-days-ago">10 Days ago</span>
+                        <span class="tweet-buttons">⚐↻❤</span>
+                      </footer>
+                    </article>`
+
+  return newTweet;
+}
+
+function renderTweets(tweets) {
+  console.log("Rendering tweets")
+  $("tweets").empty();
+  for (let tweet of tweets) {
+    console.log(tweet)
+    $("#tweets").prepend(createTweetElement(tweet));
+  }
+}
+
+function loadTweets (){
+  $.get("http://localhost:8080/tweets", (data) => {
+    console.log("logged tweets:", data);
+    renderTweets(data);
+  }) 
+}
+
+$(document).ready(() => {
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+    const formData = $("form").serialize();
+    console.log(formData);
+
+    $.post("/tweets", formData);
+    loadTweets();
+    $("#tweet-text").val("");
+  });
+
+  // renderTweets(tweetData);
+  loadTweets();
+});
