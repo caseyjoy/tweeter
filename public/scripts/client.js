3dc2bb953b4eb6ebb2c379efd0c58dc4151da5be
Casey Joy <67843601+caseyjoy@users.noreply.github.com>
2020-09-18 15:05:01 +0000

    Added some images, styles

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -46,22 +46,39 @@
 //   }
 // ]
 
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 function createTweetElement(tweet) {
-  const newTweet = `<article class="tweet">
-                      <header>
-                        <div>
-                          <img src="${tweet.user.avatars}" class="user-icon">
-                            <span class="tweet-name">${tweet.user.name}</span>
-                        </div>
-                        <span class="tweet-username">${tweet.user.handle}</span>
-                      </header>
-                      <div class="tweet-content">${tweet.content.text}</div>
-                      <footer>
-                        <span class="tweeted-how-many-days-ago">10 Days ago</span>
-                        <span class="tweet-buttons">⚐↻❤</span>
-                      </footer>
-                    </article>`
+  //console.log("AVATAR: ", $(tweet.user.avatars).text())
+  //console.log(+$(tweet.user.avatars).text());
+  const createdAt =  escape(tweet.created_at);
+  const daysAgo = Math.round((Date.now() - createdAt) / 86400000); 
+  console.log("Days ago:",createdAt, daysAgo)
+  const printableDaysAgo = `${daysAgo} ${daysAgo == 1 ? "day" : "days"} ago`;
+
+  const newTweet = 
+  `<article class="tweet">
+    <header>
+      <div>
+        <img src="${escape(tweet.user.avatars)}" class="user-icon">
+          <span class="tweet-name">${escape(tweet.user.name)}</span>
+      </div>
+      <span class="tweet-username">${escape(tweet.user.handle)}</span>
+    </header>
+    <div class="tweet-content">${escape(tweet.content.text)}</div>
+    <footer>
+      <span class="tweeted-how-many-days-ago">${printableDaysAgo}</span>
+      <span class="tweet-buttons">
+      <a class="tweet-button">⚑</a>
+      <a class="tweet-button">↻</a>
+      <a class="tweet-button">❤</a>
+      </span>
+    </footer>
+  </article>`
 
   return newTweet;
 }
@@ -88,9 +105,14 @@ $(document).ready(() => {
     const formData = $("form").serialize();
     console.log(formData);
 
-    $.post("/tweets", formData);
-    loadTweets();
-    $("#tweet-text").val("");
+    console.log(formData)
+
+    $.post("/tweets", formData, ()=>{
+      loadTweets();
+      $("#tweet-text").val("");
+    });
+   
+    
   });
 
   // renderTweets(tweetData);
